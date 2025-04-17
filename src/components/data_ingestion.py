6a78b70d75ca1b39394f844ea710d2d3af7b8d44
itsakmal <156441739+itsakmal@users.noreply.github.com>
2025-04-18 01:54:09 +0530

    Data Transformation code added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from  src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
@@ -20,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion component")
         try:
-            df=pd.read_csv('C:/Users\Mohammad Akmal/Downloads/archive (2)/StudentsPerformance.csv')
+            df=pd.read_csv('C:/Users/Mohammad Akmal/Downloads/archive (2)/StudentsPerformance.csv')
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -42,5 +45,7 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
